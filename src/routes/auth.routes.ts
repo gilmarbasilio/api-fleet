b07a13070c7fbbd85cb0f8bd511e8df556b167e2
Gilmar Basilio <gilmar.basilio@gmail.com>
2023-12-10 23:02:32 -0300

    update schema swagger

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -15,78 +15,78 @@ export const authRoutes: FastifyPluginAsync = async (
   options: FastifyPluginOptions
 ) => {
   app.route<IAddUserRequest>({
-    url: '/login',
-    method: ['POST'],
+    url: "/login",
+    method: ["POST"],
     schema: {
-      summary: 'Login a user',
-      description: 'Login a user',
-      tags: ['Auth'],
+      summary: "Login a user",
+      description: "Login a user",
+      tags: ["Auth"],
       body: {
-        type: 'object',
-        description: 'Login a user',
+        type: "object",
+        description: "Login a user",
         properties: {
           password: {
-            type: 'string'
+            type: "string",
           },
           email: {
-            type: 'string',
-            format: 'email'
-          }
-        }
+            type: "string",
+            format: "email",
+          },
+        },
       },
       response: {
         200: {
-          description: 'Login successful',
-          type: 'object',
+          description: "Login successful",
+          type: "object",
           properties: {
             token: {
-              type: 'string'
+              type: "string",
             },
-          }
+          },
         },
       },
       security: [
         {
-          bearerAuth: []
-        }
+          bearerAuth: [],
+        },
       ],
     },
     handler: loginHandler,
-  })
+  });
 
   app.route<IAddUserRequest>({
-    url: '/me',
-    method: ['GET'],
+    url: "/me",
+    method: ["GET"],
     schema: {
-      summary: 'Return a user logged data',
-      description: 'Return a user logged data',
-      tags: ['Auth'],
+      summary: "Return a user logged data",
+      description: "Return a user logged data",
+      tags: ["Auth"],
       response: {
         200: {
-          description: 'Return a user logged data',
-          type: 'object',
+          description: "Return a user logged data",
+          type: "object",
           properties: {
             id: {
-              type: 'number',
-              format: 'uuid'
+              type: "string",
+              format: "uuid",
             },
             name: {
-              type: 'string'
+              type: "string",
             },
             email: {
-              type: 'string',
-              format: 'email'
-            }
-          }
+              type: "string",
+              format: "email",
+            },
+          },
         },
       },
       security: [
         {
-          bearerAuth: []
-        }
+          bearerAuth: [],
+        },
       ],
     },
     onRequest: [app.authenticate],
     handler: userLoggedHandler,
-  })
+  });
 };
